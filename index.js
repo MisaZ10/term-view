946832f54bf07e9c530c0665df075e58ea9a092c
Misael <misaelzurdo@gmail.com>
2019-09-10 15:00:43 -0300

    Ahora se pueden entrar ir salir de una carpeta

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 'use strict'
 /* eslint new-cap: "off" */
 const blessed = require('blessed')
@@ -8,7 +9,7 @@ const createArgs = require('./lib/create-args')
 const createList = require('./lib/create-list')
 
 function handleFatalError (err) {
-  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(`${chalk.red('[fatal error]')} ${err.message} \n${err.stack}`)
   process.exit(1)
 }
 
@@ -16,60 +17,47 @@ process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
 
 const screen = blessed.screen()
-let itemSelected
 let lastKey = ''
-let listElems = []
+
+const lists = []
+let listSelected = -1
+
 const grid = new contrib.grid({
   rows: 1,
   cols: 4,
   screen
 })
+
+const box = grid.set(0, 1, 1, 3, blessed.box, {
+  content: ' *** Loading ***',
+  top: 'center',
+  left: 'center',
+  width: '5%',
+  height: '5%',
+  border: {
+    type: 'line'
+  },
+  style: {
+    focus: {
+      border: {
+        fg: '#f0f0f0'
+      }
+    }
+  }
+})
+
 const tree = grid.set(0, 0, 1, 1, contrib.tree, {
   label: 'Files List',
   keys: ['+', 'space', 'enter', 'up', 'down']
 })
 
 tree.on('select', node => {
-  itemSelected = listElems[node.index]
-  return renderBox(itemSelected)
+  const { listElems } = lists[listSelected]
+  lists[listSelected].itemSelected = listElems[node.index]
+  return renderBox()
 })
 
-screen.key(['escape', 'C-c'], (ch, key) => {
-  if (lastKey === 'C-c' && key.full === 'C-c') {
-    process.exit(0)
-  }
-  if (key.full === 'escape') {
-    process.exit(0)
-  }
-  lastKey = key.full
-})
-
-let box
-function renderBox (item) {
-  if (!box) {
-    box = grid.set(0, 1, 1, 3, blessed.box, {
-      content: item.content,
-      top: 'center',
-      left: 'center',
-      width: '5%',
-      height: '5%',
-      border: {
-        type: 'line'
-      },
-      style: {
-        focus: {
-          border: {
-            fg: '#f0f0f0'
-          }
-        }
-      }
-    })
-  }
-  box.setContent(item.content)
-  screen.render()
-}
-
-function renderTree () {
+function addList (listElems) {
   const treeData = {}
   listElems.forEach((elem, index) => {
     const type = elem.isFile ? 'File' : 'Directory'
@@ -80,7 +68,26 @@ function renderTree () {
       index
     }
   })
-  renderBox(JSON.stringify(treeData))
+  lists.push({
+    itemSelected: listElems[0],
+    treeData,
+    listElems
+  })
+  listSelected++
+  renderTree()
+}
+
+function renderBox () {
+  const { itemSelected } = lists[listSelected]
+  box.setContent(itemSelected.content)
+  screen.render()
+}
+
+function renderTree () {
+  const { treeData } = lists[listSelected]
+
+  renderBox()
+
   tree.setData({
     extended: true,
     children: treeData
@@ -91,30 +98,52 @@ function renderTree () {
 
 async function init () {
   const args = createArgs()
-  listElems = await createList(args)
-  renderTree(listElems)
+  const listElems = await createList(args)
+  addList(listElems)
   tree.focus()
   screen.render()
 }
 
 screen.key('right', async () => {
+  const { itemSelected } = lists[listSelected]
+
   if (itemSelected && itemSelected.isFile) {
     await itemSelected.nextLines()
-    renderBox(itemSelected)
+    renderBox()
   }
 })
 
 screen.key('left', async () => {
+  const { itemSelected } = lists[listSelected]
+
   if (itemSelected && itemSelected.isFile) {
     await itemSelected.previusLines()
-    renderBox(itemSelected)
+    renderBox()
   }
 })
 
 screen.key('enter', async () => {
+  const { itemSelected } = lists[listSelected]
+
   if (itemSelected && !itemSelected.isFile) {
     if (!itemSelected.contentList) await itemSelected.createContentList()
-    listElems = itemSelected.contentList;
+    addList(itemSelected.contentList)
+  }
+})
+screen.key(['escape', 'C-c'], (ch, key) => {
+  if (lastKey === 'C-c' && key.full === 'C-c') {
+    process.exit(0)
+  }
+  if (key.full === 'escape') {
+    process.exit(0)
+  }
+  lastKey = key.full
+})
+
+screen.key('backspace', (ch, key) => {
+  if (lists.length > 1) {
+    lists.pop()
+    listSelected--
     renderTree()
   }
 })
